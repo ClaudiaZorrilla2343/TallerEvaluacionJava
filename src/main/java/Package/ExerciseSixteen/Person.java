740402cfe872a3f6cdd64b4dad37ab86f5b0e2b4
ClaudiaZorrilla2343 <claudiaa2343@gmail.com>
2022-04-20 13:43:25 -0500

    Creación de la clase persona

---
+++ src/main/java/Package/ExerciseSixteen/Person.java
@@ -0,0 +1,130 @@
+package Package.ExerciseSixteen;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Person {
+
+    private String name;
+    private Integer age;
+    private Character sex;
+    private Double height;
+    private Double weight;
+    private Integer DNI;
+
+    public Person() {
+    }
+
+    public Person(String name, Integer age, Character sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.height = height;
+        this.weight = weight;
+        this.DNI = DNI;
+    }
+
+    public Person(String name, Integer age, Character sex, Double height, Double weight, Integer DNI) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.height = height;
+        this.weight = weight;
+        this.DNI = generateDNI();
+    }
+
+    public Integer calculateIMC(Double weight, Double height) {
+        Double idealWeight = Math.pow(height, 2);
+        try {
+            if (idealWeight > 20) {
+                return -1;
+            }if (idealWeight >= 20 && idealWeight <= 25) {
+                return 0;
+            }
+        }catch (Exception e){
+            System.out.println("¡ERROR!");
+        }
+        return 1;
+    }
+
+    public void checkSex(Character sex) {
+        if (sex != 'H') {
+            sex = 'H';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", DNI=" + DNI +
+                '}';
+    }
+
+    public Integer generateDNI() {
+
+        Random random = new Random();
+
+        Integer numberDNI = random.nextInt(100000000, 999999999);
+        System.out.println(numberDNI);
+
+        return numberDNI;
+    }
+
+    public void requestData() {
+
+        try {
+            Scanner read = new Scanner(System.in);
+
+            System.out.println("Ingrese su nombre: ");
+            String name = read.nextLine();
+            setName(name);
+
+            System.out.println("Ingrese su edad: ");
+            Integer age = read.nextInt();
+            setAge(age);
+
+            System.out.println("Ingrese su genero: masculino 'H' femenino 'F' ");
+            Character sex = read.nextLine().charAt(0);
+            sex = sex.toUpperCase(sex);
+            setSex(sex);
+
+            System.out.println("Ingrese su peso: ");
+            Double weight = read.nextDouble();
+            setWeight(weight);
+
+            System.out.println("Ingrese su altura: ");
+            Double height = read.nextDouble();
+            setHeight(height);
+
+        }catch (Exception e) {
+            System.out.println("¡ERROR!");
+        }
+
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setSex(Character sex) {
+        this.sex = sex;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+}
---
+++ src/main/java/Package/ExerciseSixteen/Runner.java
@@ -0,0 +1,18 @@
+package Package.ExerciseSixteen;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Person personOne = new Person();
+        Person personTwo = new Person();
+        Person personThree = new Person();
+
+        personOne.requestData();
+        personTwo.toString();
+    }
+}
